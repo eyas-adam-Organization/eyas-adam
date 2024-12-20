379dc4898e0a39b860ce36b795db2b1626d72753
eyas368 <s12112334@stu.najah.edu>
2024-12-20 20:27:58 +0200

    7th

---
+++ src/main/project/UniversalMethods.java
@@ -0,0 +1,10 @@
+public class UniversalMethods {
+    public static boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
