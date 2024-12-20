2a2dae6b3196a1466049c6a856b0bbeb6c3b0449
eyas368 <s12112334@stu.najah.edu>
2024-12-20 18:45:01 +0200

    6th

--- src/main/project/Clients.java
+++ src/main/project/Clients.java
@@ -1,12 +1,16 @@
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import io.cucumber.java.be.I;
+import io.cucumber.java.sl.In;
+
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Clients {
     String activeUser = null;
-
+     private ArrayList<Client>clients;
+     private Programs programs;
     public String clientSignUp(String username, String password) throws FileNotFoundException {
         if (userExists(username))
             return "LOGIN UNSUCCESSFUL: Username already used, try another unique username";
@@ -28,6 +32,34 @@ public class Clients {
         }
         return exists;
     }
+    public Clients() throws IOException {
+        clients=new ArrayList<>();
+        programs=new Programs();
+
+        Scanner scanner= new Scanner(new File("src/main/resources/clients_with_progress.txt"));
+        String curLine;
+        while (scanner.hasNextLine()){
+            curLine=scanner.nextLine();
+            String[]array=curLine.split(",");
+            String programTitil;
+            if(UniversalMethods.isInteger(array[0])){
+                Client client=new Client(Integer.parseInt(array[0]),"---");
+                client.setAttendanceRecord(client.getAttendanceRecord());
+                client.setCompletionRate(client.getCompletionRate());
+                client.setMotivationalReminder(client.getMotivationalReminder());
+                programTitil=array[5];
+                Program program=programs.searchForProgram(programTitil);
+                client.setProgram(program);
+                client.setNotification(client.getNotification());
+
+
+                clients.add(client);
+            }
+
+        }
+    }
+    public Programs getPrograms(){return programs;}
+
 
     public static boolean passwordInvalid(String password){
         return !(password.length() >= 8);
@@ -36,5 +68,155 @@ public class Clients {
     public String getActiveUser(){
         return this.activeUser;
     }
+
+    public String MonitorClientProgress(String id) throws FileNotFoundException {
+        if(!UniversalMethods.isInteger(id))return "Invalid client ID format";
+        else {
+            int ID= Integer.parseInt(id);
+            int CompletionRate = -2;
+            int AttendanceRecord=-2;
+
+            for(Client client:clients){
+                if(client.getID()==ID){
+                    CompletionRate=client.getCompletionRate();
+                    AttendanceRecord=client.getAttendanceRecord();
+                    break;
+                }
+            }
+            if(CompletionRate==-1||AttendanceRecord==-1)return"Progress data is incomplete for this client";
+            else if(CompletionRate==-2||AttendanceRecord==-2)return"this client does not exist";
+            else {
+                return CompletionRate+","+AttendanceRecord;
+            }
+
+        }
+
+    }
+
+    public String EditTheProgressOfAClient(String typeStr , String valueStr,String id) throws IOException {
+
+        if (!UniversalMethods.isInteger(id)) return "Invalid client ID format";
+        else {
+            int ID = Integer.parseInt(id);
+            Client client = null;
+            for (Client temp : clients) {
+                if (temp.getID() == ID) {
+                    client = temp;
+                    break;
+                }
+            }
+            if (client == null) return "This client does not exist";
+
+            if (!UniversalMethods.isInteger(typeStr)) return "invalid type";
+            else {
+                int type = Integer.parseInt(typeStr);
+                if (type > 3 || type < 1) return "invalid type";
+                if (type == 1 || type == 2) {
+                    if (!UniversalMethods.isInteger(valueStr)) return "invalid data must be a positive number";
+                    else {
+                        int value = Integer.parseInt(valueStr);
+                        if (value < 0) return "invalid data must be a positive number";
+                        else {
+                            if (type == 1) client.setNumOfDaysAttended(value);
+                            else client.setNumOfDaysMessed(value);
+                            return "Progress updated successfully";
+                        }
+                    }
+
+                }
+                if (!UniversalMethods.isInteger(valueStr)) return "invalid data must be between 0 and 100";
+                else {
+                    int value = Integer.parseInt(valueStr);
+                    if (value < 0 || value > 100) return "invalid data must be between 0 and 100";
+                    else {
+                        client.setCompletionRate(value);
+                        return "Progress updated successfully";
+                    }
+
+                }
+            }
+        }
+    }
+    private void writeToFile() throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/clients_with_progress.txt"))) {
+            for(Client client:clients){
+                String clientToFile=client.getID()+","+client.getNumOfDaysAttended()+","+client.getNumOfDaysMessed()+","+client.getCompletionRate()+","+client.getMotivationalReminder();
+                writer.write(clientToFile);
+                writer.newLine();
+            }
+        }
+
+        }
+
+    public String SendMotivationalReminder(String limit) throws IOException {
+        if(!UniversalMethods.isInteger(limit))return "wrong format of limit";
+        if(Integer.parseInt(limit)<0||Integer.parseInt(limit)>100)return "wrong format of limit";
+
+        for(Client client:clients){
+            int Limit=Integer.parseInt(limit);
+             if(client.getCompletionRate() <= Limit){
+                client.setMotivationalReminder("reserve");
+            }
+            else client.setMotivationalReminder("no reserve");
+        }
+        return "Motivational Reminder has been sent";
+
+    }
+    public ArrayList<Client>getClients(){return clients;}
+    public Client searchForClient(int ID){
+        for (Client client:clients)if(client.getID()==ID)return client;
+        return null;
+    }
+
+
+    public String NotifyClients(Program programBefore , Program programAfter) throws IOException {
+        if (programAfter == null && programBefore != null) {
+            boolean flag = false;
+            for (Client client : clients) {
+                if (client.getProgramTitle().equals(programBefore.getTitle())) {
+                    client.setNotification("remove");
+                    if (!flag) flag = true;
+                }
+            }
+            if (!flag) return "No clients are enrolled in this program";
+            return "The notification has been sent";
+        } else if (programAfter != null && programBefore !=null) {
+             if (programAfter.equals(programBefore)) return "No change to this program";
+            else {
+                boolean flag = false;
+                for (Client client : clients) {
+                     if (client.getProgramTitle().equals(programBefore.getTitle())) {
+                         client.setNotification("update");
+                         if (!flag) flag = true;
+                    }
+                }
+                if (!flag) return "No clients are enrolled in this program";
+                return "The notification has been sent";
+
+                }
+             }
+        else return "this program does not exist";
+         }
+         public String AnnounceNewProgramsOrSpecialOffers(String announcementType,String announcement) throws IOException {
+        if(announcementType.equals("new program")){
+             if(programs.searchForProgram(announcement)==null) {
+                 return "this program does not exist";
+            }
+            else {
+                for(Client client:clients) {
+                    client.setAnnouncement(announcementType + ":" + announcement);
+                 }
+                return "Announcement has been sent";
+            }
+        }
+        else if(announcementType.equals("new offer")){
+            for(Client client:clients)client.setAnnouncement(announcementType+":"+announcement);
+            return "Announcement has been sent";
+        }
+        else return "wrong Announcement";
+
+    }
+
+
 
 }
---
+++ src/main/project/Programs.java
@@ -0,0 +1,209 @@
+import io.cucumber.java.be.I;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Programs {
+    private ArrayList<Program> programs;
+    public Programs() throws FileNotFoundException, MalformedURLException {
+        programs=new ArrayList<>();
+        Scanner scanner = new Scanner(new File("src/main/resources/programs.txt"));
+        String curLine;
+        while (scanner.hasNextLine()){
+            curLine = scanner.nextLine();
+            String [] array =  curLine.split(",");
+            File video;
+            File image;
+            File doc;
+
+
+                if (array[4].equals("null")) {
+                      video = null;
+                } else {
+                     video=new File(array[4]);
+            }
+                if (array[5].equals("null")) {
+                    image = null;
+                } else {
+                     image=new File(array[5]);
+                }
+                if (array[6].equals("null")) {
+                    doc = null;
+                } else {
+                     doc=new File(array[6]);
+                }
+                Program program=new Program( array[0] , array[1],array[2],Integer.parseInt(array[3]),video,image,doc,Integer.parseInt(array[7]));
+
+                programs.add(program);
+        }
+
+    }
+    public String CreateProgram(String title,  String level, String goal , String duration, String video,  String image,  String documents, String Price)  {
+        for(Program program:programs){
+            if(program.getTitle().equals(title))return "the program is already added";
+        }
+       ArrayList<String> errorsInFiles=CheckPaths(video,image,documents);
+         if(!errorsInFiles.isEmpty()){
+             String errorMessage="";
+             for(String error:errorsInFiles)errorMessage=errorMessage+error+" & ";
+             return errorMessage.substring(0,errorMessage.length()-3);
+         }
+
+        Program program=new Program(title , level ,goal,Integer.parseInt(duration),video.equals("null")?null:new File(video),image.equals("null")?null:new File(image),documents.equals("null")?null:new File(documents), Integer.parseInt(Price));
+        String programToFile=title+","+level+","+goal+","+duration+","+video+","+image+","+documents+","+Price;
+         try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/programs.txt", true))) {
+            writer.write(programToFile);
+            writer.newLine(); // Add a new line for better formatting
+         } catch (IOException e) {
+            e.printStackTrace();
+        }
+         programs.add(program);
+         return "the program has been add successfully";
+    }
+    public String UpdateProgram(String title,  String level, String goal , String duration, String video,  String image,  String documents, String price)  {
+        boolean flag=false;
+        Program toUpdatPrograme = null;
+        for(Program program:programs){
+            if (program.getTitle().equals(title)) {
+                flag = true;
+                toUpdatPrograme=program;
+                break;
+            }
+        }
+        if(!flag)return "the program does not exist";
+        ArrayList<String> errorsInFiles=CheckPaths(video,image,documents);
+        if(!errorsInFiles.isEmpty()){
+            String errorMessage="";
+            for(String error:errorsInFiles)errorMessage=errorMessage+error+" & ";
+            return errorMessage.substring(0,errorMessage.length()-3);
+        }
+        toUpdatPrograme.setDuration(Integer.parseInt(duration));
+        toUpdatPrograme.setGoal(goal);
+        toUpdatPrograme.setPrice(Integer.parseInt(price));
+        toUpdatPrograme.setLevel(level);
+        toUpdatPrograme.setVideo(video.equals("null")?null:new File(video));
+        toUpdatPrograme.setImage(image.equals("null")?null:new File(image));
+        toUpdatPrograme.setDocuments(documents.equals("null")?null:new File(documents));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/programs.txt"))) {
+            String videoPath;
+            String docPath;
+            String imagePath;
+            for(Program program:programs){
+                if(program.getVideo()==null)videoPath="null";
+                else videoPath=program.getVideo().getPath();
+                if(program.getImage()==null)imagePath="null";
+                else imagePath=program.getImage().getPath();
+                if(program.getDocuments()==null)docPath="null";
+                else docPath=program.getDocuments().getPath();
+
+                String programToFile=program.getTitle()+","+program.getLevel()+","+program.getGoal()+","+program.getDuration()+","+videoPath+","+imagePath+","+docPath+","+program.getPrice();
+                writer.write(programToFile);
+                writer.newLine(); // Add a new line for better formatting
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "the program has been updated successfully";
+    }
+
+    public String DeleteProgram(String title)  {
+        boolean flag=false;
+        Program toDeletePrograme = null;
+        for(Program program:programs){
+            if (program.getTitle().equals(title)) {
+                flag = true;
+                toDeletePrograme=program;
+                break;
+            }
+        }
+        if(!flag)return "the program does not exist";
+        programs.remove(toDeletePrograme);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/programs.txt"))) {
+            String videoPath;
+            String docPath;
+            String imagePath;
+            for(Program program:programs){
+                if(program.getVideo()==null)videoPath="null";
+                else videoPath=program.getVideo().getPath();
+                if(program.getImage()==null)imagePath="null";
+                else imagePath=program.getImage().getPath();
+                if(program.getDocuments()==null)docPath="null";
+                else docPath=program.getDocuments().getPath();
+
+                String programToFile=program.getTitle()+","+program.getLevel()+","+program.getGoal()+","+program.getDuration()+","+videoPath+","+imagePath+","+docPath+","+program.getPrice();
+                writer.write(programToFile);
+                writer.newLine(); // Add a new line for better formatting
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "the program has been removed successfully";
+    }
+    private ArrayList<String> CheckPaths(String video,  String image,  String documents){
+        ArrayList<String>errorsInFiles=new ArrayList<>();
+        boolean videoFlag = false;
+        boolean imageFlag = false;
+        boolean docFlag = false;
+        try {
+            // Check if the path exists and points to a file
+            videoFlag=Files.exists(Paths.get(video)) && Files.isRegularFile(Paths.get(video));
+        } catch (Exception e) {
+            // Catch exceptions (e.g., invalid paths)
+            errorsInFiles.add("video/wrong file");
+        }
+        if(videoFlag){
+            String extension=video.split("\\.")[1];
+            if(!(extension.equals("mp4")||extension.equals("webm")))errorsInFiles.add("video/wrong extension");
+        }
+        else if(!errorsInFiles.contains("video/wrong file")) errorsInFiles.add("video/wrong file");
+        if(video.equals("null"))errorsInFiles.remove(0);
+
+
+
+        try {
+            // Check if the path exists and points to a file
+            imageFlag=Files.exists(Paths.get(image)) && Files.isRegularFile(Paths.get(image));
+        } catch (Exception e) {
+            // Catch exceptions (e.g., invalid paths)
+            errorsInFiles.add("image/wrong file");
+        }
+        if(imageFlag){
+            String extension=image.split("\\.")[1];
+            if(!(extension.equals("png")||extension.equals("jpeg")||extension.equals("jpg")))errorsInFiles.add("image/wrong extension");
+        }
+        else if(!errorsInFiles.contains("image/wrong file"))  errorsInFiles.add("image/wrong file");
+        if(image.equals("null"))errorsInFiles.remove(errorsInFiles.size()-1);
+
+
+
+        try {
+            // Check if the path exists and points to a file
+            docFlag=Files.exists(Paths.get(documents)) && Files.isRegularFile(Paths.get(documents));
+
+        } catch (Exception e) {
+            // Catch exceptions (e.g., invalid paths)
+            errorsInFiles.add("doc/wrong file");
+        }
+        if(!docFlag && !errorsInFiles.contains("doc/wrong file"))errorsInFiles.add("doc/wrong file");
+        if(documents.equals("null"))errorsInFiles.remove(errorsInFiles.size()-1);
+
+        return errorsInFiles;
+
+    }
+    public Program searchForProgram(String title){
+        for (Program program:programs)if(program.getTitle().equals(title))return program;
+        return null;
+    }
+    public ArrayList<Program> getPrograms(){return  programs;}
+
+
+}
